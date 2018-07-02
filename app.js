e49a223aa2679eeddaeada58e4221ef75ee38104
cygas <35080448+cygas@users.noreply.github.com>
2018-07-02 15:11:04 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-window.addEventListener("load", function(){
+(function(global){
 	
-	window.MainContainer = new App();
+	global.App = function({height, side}){
+		
+		this.init = function(){
+			this.config = {
+				chessBoard: document.getElementById("board"),
+				rowClass: "row",
+				cellClass: "cell",
+				height: height,
+				side: side,
+			};
+			this.board = new Board(this.config);				
+		};		
+		this.init();		
+	};
 	
-});
+})(window);
