fb4cc64c7a2ee8dfbbc2152c9746dc17c33abaf0
cygas <35080448+cygas@users.noreply.github.com>
2018-07-03 15:13:01 +0200

    Add files via upload

--- board.js
+++ board.js
@@ -1,43 +1,44 @@
 (function(global){
 	
 	global.Board = function(config){
-		this.init = function(){
-			this.divCreator = {
-				side: null,
-				nameClass: null,
-				id: null,			
-				height: null,
-				width: null,
-				increment: 0,
-				createDiv: function(parent){
-					let div = document.createElement("div");
-					div.id = this.id;
-					div.className = this.nameClass;				
-					div.style.width = this.width + "px";
-					div.style.height = this.height + "px";
-					parent.appendChild(div);				
-				},
-				createBoard: function(){
-					
-					for(let i=0; i<config.side; i++){			
-						this.nameClass = config.rowClass;
-						this.id = config.rowClass + i;	
-						this.height = config.height;		
-						this.width = config.height*config.side;		
-						this.createDiv(config.chessBoard);
-						for(let k=0; k< config.side; k++){
-							this.nameClass = config.cellClass;
-							this.id = config.cellClass + this.increment;	
-							this.height = config.height;		
-							this.width = config.height;		
-							this.createDiv(document.getElementById(config.rowClass + i));
-							this.increment++;
-						}
+		this.init = function(obj){
+			this.divCreator.createBoard(obj)
+		};
+		
+		this.divCreator = {
+			side: null,
+			nameClass: null,
+			id: null,			
+			height: null,
+			width: null,
+			increment: 0,
+			createDiv: function(parent){
+				let div = document.createElement("div");
+				div.id = this.id;
+				div.className = this.nameClass;				
+				div.style.width = this.width + "px";
+				div.style.height = this.height + "px";
+				parent.appendChild(div);				
+			},
+			createBoard: function(obj){					
+				for(let i=0; i<obj.side; i++){			
+					this.nameClass = obj.rowClass;
+					this.id = obj.rowClass + i;	
+					this.height = obj.height;		
+					this.width = obj.height*obj.side;		
+					this.createDiv(obj.chessBoard);
+					for(let k=0; k< obj.side; k++){
+						this.nameClass = obj.cellClass;
+						this.id = obj.cellClass + this.increment;	
+						this.height = obj.height;		
+						this.width = obj.height;		
+						this.createDiv(document.getElementById(obj.rowClass + i));
+						this.increment++;
 					}
-					
-				}
-			};		
-		}.call(this);		
+				}					
+			}
+		};
+		this.init(config);
 	};
 	
-})(this);
+})(window);
--- player.js
+++ player.js
@@ -1,73 +1,88 @@
 (function(global){
 	
 	global.Player = function(side){
-		
+				
 		this.init = function(){
-			this.pieces = ["king", "queen", "knight", "rook", "bishop", "pawn"];
+			this.pieces = [];				
+			this.position = [];
 			this.white = "W";
-			this.black = "B";
-			this.cell13 = document.getElementById("cell13");
+			this.black = "B";			
 			this.num = side;
-		}.call(this);
-		
-		this.createPiece = function(nr, color, el){
-			let piece = "url(img/" + this.pieces[nr] + color + ".png)";
+			this.pieceNum = 16; //number of pieces in chess game
+			
+			this.createPiece(this.pieces, this.num).randomPlace(this.num, this.position);
+			//this.randomPlace(this.num, this.position);
+			this.displayPiece(this.position, this.pieces, this.black).showMoves(this.pieces);
+			//this.showMoves(this.pieces);
+		};
+
+		this.createPiece = function(arr, num){
+			this.king = Piece.factory("king", num);
+			this.queen = Piece.factory("queen", num);
+			this.knight1 = Piece.factory("knight", num);
+			this.knight2 = Piece.factory("knight", num);
+			this.rook1 = Piece.factory("rook", num);
+			this.rook2 = Piece.factory("rook", num);
+			this.bishop1 = Piece.factory("bishop", num);
+			this.bishop2 = Piece.factory("bishop", num);
+			this.pawn1 = Piece.factory("pawn", num);
+			this.pawn2 = Piece.factory("pawn", num);
+			this.pawn3 = Piece.factory("pawn", num);
+			this.pawn4 = Piece.factory("pawn", num);
+			this.pawn5 = Piece.factory("pawn", num);
+			this.pawn6 = Piece.factory("pawn", num);
+			this.pawn7 = Piece.factory("pawn", num);
+			this.pawn8 = Piece.factory("pawn", num);
+			
+			arr.push(this.king, this.queen, this.knight1, this.knight2, this.rook1, this.rook2, this.bishop1, this.bishop2, this.pawn1, this.pawn2, this.pawn3, this.pawn4, this.pawn5, this.pawn6, this.pawn7, this.pawn8); 
+			return this;
+		};		
+		this.randomPlace = function(side, pos){
+			while(pos.length < 2*side && pos.length<this.pieceNum){
+				let randomnumber = Math.floor(Math.random()*side*side);
+				if(pos.indexOf(randomnumber) > -1) continue;
+				pos.push(randomnumber);
+			}
+			return this;
+		};			
+		this.addPiece = function(obj, color, el){
+			let piece = "url(img/" + obj.name + color + ".png)";
 			el.style.backgroundImage = piece;
 			el.style.backgroundSize = "contain";
-		}.bind(this);
-		
-		
-		this.createPlayer = function(color){
-			if(color == "W"){
-				for(let i=0; i<this.num*2; i++){
-					let el = document.getElementById("cell" + i);
-					if(i == 0 || i == 7){
-						this.createPiece(3, color, el);
-					}
-					if(i == 1 || i == 6){
-						this.createPiece(2, color, el);
-					}
-					if(i == 2 || i == 5){
-						this.createPiece(4, color, el);
-					}
-					if(i == 3){
-						this.createPiece(0, color, el);
-					}
-					if(i == 4){
-						this.createPiece(1, color, el);
-					}
-					if( i > 7 && i < 16){
-						this.createPiece(5, color, el);
-					}
+			return this;
+		};	
+		this.displayPiece = function(pos, arr, color){
+			for(let i=0; i<pos.length;i++){
+				let el = document.getElementById("cell" + pos[i]);
+				this.addPiece(arr[i], color, el);
+				arr[i].pos = pos[i];
+			}
+			return this;
+		};
+		this.showMoves = function(arr){
+			for(let i=0;i<arr.length;i++){
+				if(arr[i].pos!==null){
+					let cell = document.getElementById("cell" + arr[i].pos);
+					arr[i].moves = checkPos(side, arr[i]);
+					cell.addEventListener("click", function(){	
+						cell.classList.toggle("clickCell");
+						cell.classList.toggle("cell");
+						arr[i].moves.forEach(function(item){
+							let moveCell = document.getElementById("cell" + item);
+							moveCell.classList.toggle("moveCell");
+						}.bind(this));
+						
+					}.bind(this));
 				}
 			}
-			if(color == "B"){			
-				for(let i=0; i<this.num*2; i++){	
-					let el = document.getElementById("cell" + (this.num*this.num-1-i));					
-					if(i == 0 || i == 7){
-						this.createPiece(3, color, el);
-					}
-					if(i == 1 || i == 6){
-						this.createPiece(2, color, el);
-					}
-					if(i == 2 || i == 5){
-						this.createPiece(4, color, el);
-					}
-					if(i == 3){
-						this.createPiece(0, color, el);
-					}
-					if(i == 4){
-						this.createPiece(1, color, el);
-					}
-					if( i > 7 && i < 16){
-						this.createPiece(5, color, el);
-					}
-				}
-			}
-		}.bind(this);
+			return this;
+		};		
 		
-		this.createPlayer(this.white);
-		this.createPlayer(this.black);
+		this.init();	
+		
+		console.log(this.queen);
+		console.log(this.rook1);
+		console.log(this.bishop2);		
 	};
 	
 })(this);
